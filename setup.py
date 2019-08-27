b1cd01b2ae2f893211d5d80b7d0c50787d9d8a6e
Gihwan Kim <ghkim3221@gmail.com>
2019-08-27 22:40:56 +0900

    chore: setuptools로 프로젝트 초기화

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="boj",
+    version="0.0.1",
+    url="https://github.com/ghkim3221/boj",
+    author="Gihwan Kim",
+    author_email="ghkim3221@gmail.com",
+    license="MIT",
+    description="깔끔한 Baekjoon Online Judge 코드 관리를 위한 커맨드 라인 명령어",
+    packages=[
+        'boj'
+    ],
+    entry_points={
+        'console_scripts': [
+            'boj = boj.main:main'
+        ]
+    },
+)
