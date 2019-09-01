3ca7477561242692f01760ab84a68113568d6c4a
Gihwan Kim <ghkim3221@gmail.com>
2019-09-01 19:08:07 +0900

    refactor: 웹사이트에서 문제 데이터 엑세스하는 함수 추출

--- boj/problem.py
+++ boj/problem.py
@@ -2,63 +2,32 @@ import datetime
 import os
 import sys
 
+from typing import List
+
 import bs4
 import requests
 
 from boj import selector
 from boj.language import C, CPP, PYTHON
-from boj.sample import parse_sample
+from boj.sample import Sample, parse_sample
 
 
 class Problem:
-    def __init__(self, number: int, language: str):
-        self.number = number
+    def __init__(self,
+                 language: str,
+                 number: int,
+                 title: str,
+                 url: str,
+                 time_limit: str,
+                 memory_limit: str,
+                 samples: List[Sample]):
         self.language = language
-        self.url = 'https://www.acmicpc.net/problem/{}'.format(number)
-        self.title = None
-        self.time_limit = None
-        self.memory_limit = None
-        self.samples = []
-
-    def _fetch(self) -> None:
-        """
-        Baekjoon Online Judge 웹사이트의 문제 번호에 해당하는 문제의 데이터를 
-        가져온다. 가져오는 데이터의 종류는 다음과 같다.
-
-        * 문제 이름
-        * 시간 제한
-        * 메모리 제한
-        * 예제 입력
-        * 예제 출력
-        """
-        response = requests.get(self.url)
-        if not response.ok:
-            raise Exception()
-
-        html = response.text
-        soup = bs4.BeautifulSoup(html, 'html.parser')
-
-        self.title = soup.select_one(selector.title()).string
-        self.time_limit = soup.select_one(selector.time_limit()).string
-        self.memory_limit = soup.select_one(selector.memory_limit()).string
-
-        sample_number = 1
-        while True:
-            sample_input_selector = selector.sample_input(sample_number)
-            sample_output_selector = selector.sample_output(sample_number)
-
-            sample_input_tag = soup.select_one(sample_input_selector)
-            sample_output_tag = soup.select_one(sample_output_selector)
-
-            if (sample_input_tag is None) and(sample_output_tag is None):
-                break
-            else:
-                sample = parse_sample(sample_number,
-                                      sample_input_tag,
-                                      sample_output_tag)
-                self.samples.append(sample)
-
-                sample_number += 1
+        self.number = number
+        self.title = title
+        self.url = url
+        self.time_limit = time_limit
+        self.memory_limit = memory_limit
+        self.samples = samples
 
     def _comment_begin(self) -> str:
         """
@@ -77,8 +46,6 @@ class Problem:
         문제를 파일에 쓴다.
         파일 이름은 {문제 번호}.{언어} 이다.
         """
-        self._fetch()
-
         # TODO(@ghkim3221): 순환 참조. 파일에 쓰는 부분을 외부 모듈로 옮겨야 함.
         from boj.header import make_header
         header = make_header(self)
@@ -90,8 +57,20 @@ class Problem:
 
 
 class CProblem(Problem):
-    def __init__(self, number: int):
-        super(CProblem, self).__init__(number, C)
+    def __init__(self,
+                 number: int,
+                 title: str,
+                 url: str,
+                 time_limit: str,
+                 memory_limit: str,
+                 samples: List[Sample]):
+        super(CProblem, self).__init__(C,
+                                       number,
+                                       title,
+                                       url,
+                                       time_limit,
+                                       memory_limit,
+                                       samples)
 
     def _comment_begin(self) -> str:
         return '/*'
@@ -101,8 +80,20 @@ class CProblem(Problem):
 
 
 class CppProblem(Problem):
-    def __init__(self, number: int):
-        super(CppProblem, self).__init__(number, CPP)
+    def __init__(self,
+                 number: int,
+                 title: str,
+                 url: str,
+                 time_limit: str,
+                 memory_limit: str,
+                 samples: List[Sample]):
+        super(CppProblem, self).__init__(CPP,
+                                         number,
+                                         title,
+                                         url,
+                                         time_limit,
+                                         memory_limit,
+                                         samples)
 
     def _comment_begin(self) -> str:
         return '/*'
@@ -112,8 +103,20 @@ class CppProblem(Problem):
 
 
 class PythonProblem(Problem):
-    def __init__(self, number: int):
-        super(PythonProblem, self).__init__(number, PYTHON)
+    def __init__(self,
+                 number: int,
+                 title: str,
+                 url: str,
+                 time_limit: str,
+                 memory_limit: str,
+                 samples: List[Sample]):
+        super(PythonProblem, self).__init__(PYTHON,
+                                            number,
+                                            title,
+                                            url,
+                                            time_limit,
+                                            memory_limit,
+                                            samples)
 
     def _comment_begin(self) -> str:
         return '"""'
@@ -140,6 +143,98 @@ def parse_language(language: str) -> str:
         return PYTHON
 
 
+def _make_url(number: int) -> str:
+    return 'https://www.acmicpc.net/problem/{}'.format(number)
+
+
+def _parse_title(soup: bs4.BeautifulSoup) -> str:
+    title_tag = soup.select_one(selector.title())
+    if title_tag is None:
+        return None
+
+    raw_title = title_tag.string
+    if raw_title is None:
+        return None
+
+    title = raw_title.strip()
+    return title
+
+
+def _parse_time_limit(soup: bs4.BeautifulSoup) -> str:
+    time_limit_tag = soup.select_one(selector.time_limit())
+    if time_limit_tag is None:
+        return None
+
+    raw_time_limit = time_limit_tag.string
+    if raw_time_limit is None:
+        return None
+
+    time_limit = raw_time_limit.strip()
+    return time_limit
+
+
+def _parse_memory_limit(soup: bs4.BeautifulSoup) -> str:
+    memory_limit_tag = soup.select_one(selector.memory_limit())
+    if memory_limit_tag is None:
+        return None
+
+    raw_memory_limit = memory_limit_tag.string
+    if raw_memory_limit is None:
+        return None
+
+    memory_limit = raw_memory_limit.strip()
+    return memory_limit
+
+
+def _parse_samples(soup: bs4.BeautifulSoup) -> List[Sample]:
+    samples = []
+
+    number = 1
+    while True:
+        sample_input_selector = selector.sample_input(number)
+        sample_input_tag = soup.select_one(sample_input_selector)
+
+        sample_output_selector = selector.sample_output(number)
+        sample_output_tag = soup.select_one(sample_output_selector)
+
+        if (sample_input_tag is None) and (sample_output_tag is None):
+            break
+
+        sample = parse_sample(number, sample_input_tag, sample_output_tag)
+        samples.append(sample)
+
+        number += 1
+
+    return samples
+
+
+def _fetch_problem(number: int, language: str) -> Problem:
+    url = _make_url(number)
+    response = requests.get(url)
+    if not response.ok:
+        raise Exception()
+
+    html = response.text
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+
+    title = _parse_title(soup)
+    time_limit = _parse_time_limit(soup)
+    memory_limit = _parse_memory_limit(soup)
+    samples = _parse_samples(soup)
+
+    problem_class = None
+    if language == C:
+        problem_class = CProblem
+    elif language == CPP:
+        problem_class = CppProblem
+    elif language == PYTHON:
+        problem_class = PythonProblem
+
+    if problem_class is None:
+        raise Exception()
+    return problem_class(number, title, url, time_limit, memory_limit, samples)
+
+
 def create_problem(number: int, raw_language: str) -> Problem:
     """
     language에 해당하는 문제 객체를 생성한다.
@@ -158,9 +253,4 @@ def create_problem(number: int, raw_language: str) -> Problem:
         sys.stderr.write(os.linesep)
         sys.exit(1)
 
-    if language == C:
-        return CProblem(number)
-    elif language == CPP:
-        return CppProblem(number)
-    elif language == PYTHON:
-        return PythonProblem(number)
+    return _fetch_problem(number, language)
