3cd24650677fdbd22ae9513d418e0aac1765f5c0
Gihwan Kim <ghkim3221@gmail.com>
2019-09-15 20:05:08 +0900

    feat: 문제 생성 완료 출력할 때 문제 이름도 같이 출력

--- boj/main.py
+++ boj/main.py
@@ -26,8 +26,8 @@ def new(number: str, language: str) -> None:
     problem = create_problem(number, language)
     problem.write()
     print(
-        "{} 언어로 문제 {} 를(을) 생성하였습니다.".format(
-            problem.language, problem.number
+        "{} 언어로 문제 {} ({}) 를(을) 생성하였습니다.".format(
+            problem.language, problem.number, problem.title,
         )
     )
 
